0330a215cda17801fd94224a01acc1eceda33fff
Bwiza-Nina <ninamyles2020@gmail.com>
2022-12-13 09:46:58 +0200

    Setup to get a clean project

--- simple/src/App.js
+++ simple/src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Header from "./Components/Header";
+import Calculator from "./Components/Calculator";
+import * as math from "mathjs";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      result: "Welcome!"
+    };
+  }
+
+  addToResult = val => {
+    this.setState({
+      result: this.state.result + val
+    });
+  };
+
+  equal = () => {
+    try {
+      this.setState({
+        result: math.evaluate(this.state.result)
+      });
+    } catch (e) {
+      this.setState({
+        result: "Error!"
+      });
+      setTimeout(
+        () =>
+          this.setState({
+            result: ""
+          }),
+        1000
+      );
+    }
+  };
+
+  clear = () => {
+    this.setState({
+      result: ""
+    });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <Header />
+        <Calculator
+          resultLabel={this.state.result}
+          addToResult={this.addToResult}
+          clear={this.clear}
+          equal={this.equal}
+        />
+      </React.Fragment>
+    );
+  }
 }
-
-export default App;
