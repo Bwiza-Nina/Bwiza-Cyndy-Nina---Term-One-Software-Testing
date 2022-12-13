5db1dbbe1d2654328b63a1cd6f8fc7645ef4e581
Bwiza-Nina <ninamyles2020@gmail.com>
2022-12-13 09:33:26 +0200

    Initial commit

---
+++ simple/src/Components/Calculator.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const useStyles = makeStyles({
+    root:{
+        margin: "10px 10px",
+        padding: "20px"
+    },
+    resultField: {
+        width: "100%"
+    },
+    keypad: {
+        width: "100%"
+    }
+});
+
+export const Calculator = () => {
+  return (
+    <>
+
+    </>
+  )
+}
